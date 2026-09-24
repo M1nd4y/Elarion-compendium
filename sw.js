903581f666cf905459798e89e83a26bcb20e9388
M1nd4y <83182156+M1nd4y@users.noreply.github.com>
2026-09-23 21:08:11 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'elarion-v1';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './styles/style.css',
+  './audio/musica.mp3',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Instalar y guardar todo en caché
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+// Activar
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Servir desde caché (funciona offline)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
